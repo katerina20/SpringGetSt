8685fdc711323d8c6222d58fe03ec7a188f712b4
Katerina <malutina.catya14@gmail.com>
2019-10-21 08:49:37 +0300

    Controller and jsp file "index" were edit and connected

--- src/main/java/com/netcr/controller/HW.java
+++ src/main/java/com/netcr/controller/HW.java
@@ -1,4 +1,12 @@
 package com.netcr.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HW {
+    @GetMapping("/")
+    public String firstPage() {
+        return "index";
+    }
 }
